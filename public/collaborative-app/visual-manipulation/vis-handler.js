08c3b98f50a91699d823ea41d070e28de688043a
fdurupinar <f.durupinar@gmail.com>
2019-01-09 19:13:00 -0800

    select all possible elements

--- public/collaborative-app/visual-manipulation/vis-handler.js
+++ public/collaborative-app/visual-manipulation/vis-handler.js
@@ -59,7 +59,7 @@ VisHandler.prototype.findAllNodesFromLabel = function(name,  nodes){
 
 VisHandler.prototype.findNodeFromLabel = function(name, state, nodes) {
 
-    let myNode = null;
+    let myNodes = [];
 
     let possibleNodes = [];
 
@@ -79,7 +79,7 @@ VisHandler.prototype.findNodeFromLabel = function(name, state, nodes) {
     });
 
     if(!state && possibleNodes.length > 0){ //no need to compare
-            myNode = possibleNodes[0];
+            return possibleNodes;
     }
     else{
 
@@ -95,14 +95,14 @@ VisHandler.prototype.findNodeFromLabel = function(name, state, nodes) {
 
                     if (value && value.toLowerCase() === state.toLowerCase() ||
                         value && possibleStates[state.toLowerCase()] && possibleStates[state.toLowerCase()].indexOf(value.toLowerCase()) > -1 || !value && state === '') //if any state matches this
-                        myNode = node;
+                        myNodes.push(node);
                 }
             }
 
         });
     }
 
-    return myNode;
+    return myNodes;
 }
 
 
@@ -134,7 +134,7 @@ VisHandler.prototype.moveNodeAndChildren = function(positionDiff, node, cyId) {
  */
 VisHandler.prototype.moveNode = function(data) {
 
-    let nodeToMove;
+    let nodesToMove;
     let posToMove;
 
     let name = data.name;
@@ -150,54 +150,11 @@ VisHandler.prototype.moveNode = function(data) {
 
     let nodes = appUtilities.getCyInstance(data.cyId).nodes();
 
-    nodeToMove = this.findNodeFromLabel(name, state, nodes);
+    nodesToMove = this.findNodeFromLabel(name, state, nodes);
 
-    //move our node first
+    nodesToMove.forEach((nodeToMove) => {
 
-    let bBox = cy.elements().boundingBox();
-
-    //extend bbox
-    bBox.x1 -= extensionX;
-    bBox.x2 += extensionX;
-    bBox.y1 -= extensionY;
-    bBox.y2 += extensionY;
-
-
-    if(location.toUpperCase().indexOf('TOP')> -1)
-        posToMove = {x: (bBox.x1 + bBox.x2) / 2, y: bBox.y1};
-    else if(location.toUpperCase().indexOf('BOTTOM')> -1)
-        posToMove = {x: (bBox.x1 + bBox.x2) / 2, y: bBox.y2};
-    else if(location.toUpperCase().indexOf('LEFT')> -1)
-        posToMove = {x: bBox.x1, y: (bBox.y1 + bBox.y2) / 2};
-    else if(location.toUpperCase().indexOf('RIGHT')> -1)
-        posToMove = {x: bBox.x2, y: (bBox.y1 + bBox.y2) / 2};
-
-
-    //move node -- no need to update the model for now
-    nodeToMove.position(posToMove);
-
-    if(nodeToMove.isParent()){
-        let posDiff = {x: posToMove.x - nodeToMove.position("x"), y: posToMove.y - nodeToMove.position("y")};
-        this.moveNodeAndChildren(posDiff, nodeToMove);
-    }
-
-    //make sure model is updated accordingly
-   this.modelManager.changeModelNodeAttribute("position", nodeToMove.data("id"), data.cyId, posToMove, "me");
-   
-
-
-   nodeToMove.lock();
-
-
-    $("#perform-layout").trigger('click');
-
-    let layoutCose = cy.layout({'name': 'cose', idealEdgeLength:5, edgeElasticity:5});
-    layoutCose.run();
-
-    cy.on('layoutstop', () => {
-        nodeToMove.unlock();
-
-        //move again
+        //move our node first
 
         let bBox = cy.elements().boundingBox();
 
@@ -207,26 +164,71 @@ VisHandler.prototype.moveNode = function(data) {
         bBox.y1 -= extensionY;
         bBox.y2 += extensionY;
 
-        if(location.toUpperCase().indexOf('TOP')> -1)
+
+        if (location.toUpperCase().indexOf('TOP') > -1)
             posToMove = {x: (bBox.x1 + bBox.x2) / 2, y: bBox.y1};
-        else if(location.toUpperCase().indexOf('BOTTOM')> -1)
+        else if (location.toUpperCase().indexOf('BOTTOM') > -1)
             posToMove = {x: (bBox.x1 + bBox.x2) / 2, y: bBox.y2};
-        else if(location.toUpperCase().indexOf('LEFT')> -1)
+        else if (location.toUpperCase().indexOf('LEFT') > -1)
             posToMove = {x: bBox.x1, y: (bBox.y1 + bBox.y2) / 2};
-        else if(location.toUpperCase().indexOf('RIGHT')> -1)
+        else if (location.toUpperCase().indexOf('RIGHT') > -1)
             posToMove = {x: bBox.x2, y: (bBox.y1 + bBox.y2) / 2};
 
-
-        if(nodeToMove.isParent()){
-            let posDiff = {x: posToMove.x - nodeToMove.position("x"), y: posToMove.y - nodeToMove.position("y")};
-            this.moveNodeAndChildren(posDiff, nodeToMove);
-        }
 
         //move node -- no need to update the model for now
         nodeToMove.position(posToMove);
 
+        if (nodeToMove.isParent()) {
+            let posDiff = {x: posToMove.x - nodeToMove.position("x"), y: posToMove.y - nodeToMove.position("y")};
+            this.moveNodeAndChildren(posDiff, nodeToMove);
+        }
+
         //make sure model is updated accordingly
         this.modelManager.changeModelNodeAttribute("position", nodeToMove.data("id"), data.cyId, posToMove, "me");
+
+
+        nodeToMove.lock();
+
+
+        $("#perform-layout").trigger('click');
+
+        let layoutCose = cy.layout({'name': 'cose', idealEdgeLength: 5, edgeElasticity: 5});
+        layoutCose.run();
+
+        cy.on('layoutstop', () => {
+            nodeToMove.unlock();
+
+            //move again
+
+            let bBox = cy.elements().boundingBox();
+
+            //extend bbox
+            bBox.x1 -= extensionX;
+            bBox.x2 += extensionX;
+            bBox.y1 -= extensionY;
+            bBox.y2 += extensionY;
+
+            if (location.toUpperCase().indexOf('TOP') > -1)
+                posToMove = {x: (bBox.x1 + bBox.x2) / 2, y: bBox.y1};
+            else if (location.toUpperCase().indexOf('BOTTOM') > -1)
+                posToMove = {x: (bBox.x1 + bBox.x2) / 2, y: bBox.y2};
+            else if (location.toUpperCase().indexOf('LEFT') > -1)
+                posToMove = {x: bBox.x1, y: (bBox.y1 + bBox.y2) / 2};
+            else if (location.toUpperCase().indexOf('RIGHT') > -1)
+                posToMove = {x: bBox.x2, y: (bBox.y1 + bBox.y2) / 2};
+
+
+            if (nodeToMove.isParent()) {
+                let posDiff = {x: posToMove.x - nodeToMove.position("x"), y: posToMove.y - nodeToMove.position("y")};
+                this.moveNodeAndChildren(posDiff, nodeToMove);
+            }
+
+            //move node -- no need to update the model for now
+            nodeToMove.position(posToMove);
+
+            //make sure model is updated accordingly
+            this.modelManager.changeModelNodeAttribute("position", nodeToMove.data("id"), data.cyId, posToMove, "me");
+        });
     });
 }
 
@@ -257,9 +259,15 @@ VisHandler.prototype.selectNodeStream = function(data){
     let cy = appUtilities.getCyInstance(data.cyId);
 
 
-    let nodeId = this.findNodeFromLabel(data.name, data.state, cy.nodes()).data("id");
+    let nodes = this.findNodeFromLabel(data.name, data.state, cy.nodes());
 
-    let nodeIds = this.findStream(nodeId, data.direction, cy);
+    let ids = [];
+    nodes.forEach(function(node){
+        ids.push(node.data("id"));
+    });
+
+
+    let nodeIds = this.findStream(ids, data.direction, cy);
 
     //unselect all first
     cy.elements().unselect();
@@ -348,26 +356,28 @@ VisHandler.prototype.moveSelected = function(nodesSelected, data){
  * @param direction: "up" or "down"
  * @param cy
  */
-VisHandler.prototype.findStream = function(nodeId, direction, cy){
+VisHandler.prototype.findStream = function(ids, direction, cy){
 
     let self = this;
     let nodeIds = [];
 
     let directionMap = {down: {node: "source", neighbor:"target"}, up: {node: "target", neighbor:"source"} };
 
+    ids.forEach(function(nodeId)  {
 
-    let edgeStr = "[" + directionMap[direction].node  +"='" + nodeId + "']";
+        let edgeStr = "[" + directionMap[direction].node + "='" + nodeId + "']";
 
-    let edges = cy.edges(edgeStr);
+        let edges = cy.edges(edgeStr);
 
 
-    edges.forEach(function(edge){
-        let neighborId = edge.data(directionMap[direction].neighbor);
+        edges.forEach(function (edge) {
+            let neighborId = edge.data(directionMap[direction].neighbor);
 
-        let nextLevelNodeIds = self.findStream(neighborId, direction, cy);
+            let nextLevelNodeIds = self.findStream([neighborId], direction, cy);
 
-        nodeIds = nodeIds.concat(nextLevelNodeIds);
-        nodeIds.push(neighborId);
+            nodeIds = nodeIds.concat(nextLevelNodeIds);
+            nodeIds.push(neighborId);
+        });
     });
 
     return nodeIds;
